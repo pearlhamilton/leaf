858439838919be2344a4eb185d6ac1b2d6281903
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-19 14:07:54 +0100

    add basic styling to items

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,16 +1,21 @@
 import React from "react"
+import './style.css'
 
 
 const Item = (props) => {
 
     console.log(props.productData.id)
     return(
-        <>
+        <div className="item">
+
         <h4>{props.productData.product}</h4>
         <h4>{props.productData.description}</h4>
         <h4>£{props.productData.price}</h4>
         <img src={props.productData.img}></img>
-        </>
+        <button>Add to Cart</button>
+        <button>View item</button>
+        </div>
+
 
     )
 }
